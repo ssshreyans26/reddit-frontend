ea64270d7afa5c818a1c8a22432bf0725e9aa341
Shreyans Sonthalia <ssshreyans26@gmail.com>
2021-03-14 21:45:58 +0530

    some more chanfes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import CreatePost from "./components/CreatePost";
+import GoogleLogin from "./components/GoogleLogin"
 
 function App() {
   return (
@@ -8,7 +9,9 @@ function App() {
       <BrowserRouter>
       <Link to="">DashBoard</Link>
       <Link to="/createpost">Create Post</Link>
+      <Link to="/googlelogin">Google Login</Link>
 
+      <Route exact path="/googlelogin" component={GoogleLogin}/>
       <Route exact path="/" component={Dashboard}/>
       <Route path="/createpost" component={CreatePost}/>
       </BrowserRouter>
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -36,24 +36,27 @@ export default class Dashboard extends Component {
             
           <Card border="light" bg="dark" text="light">
             <Card.Header as="h3" className="">
-              {post.uid}
+              {post.test.displayName}
+              
             </Card.Header>
             <Card.Body>
               <Card.Title>{post.caption}</Card.Title>
               {/* <Card.Img variant="top" src="{post.Location}" /> */}
               <Card.Text>{post.desc}</Card.Text>
               {/* <Button variant="light">Go somewhere</Button> */}
+              
               <FontAwesomeIcon
                 className="mr-1"
                 size="2x"
                 icon={faArrowCircleUp}
               />
+              <span className="text-center mx-2 mb-2">{post.votes}</span>
               <FontAwesomeIcon
                 className="mr-1"
                 size="2x"
                 icon={faArrowCircleDown}
               />
-              <FontAwesomeIcon size="2x" icon={faCommentDots} />
+              <FontAwesomeIcon className="ml-0" size="2x" icon={faCommentDots} />
             </Card.Body>
             <Card.Footer className="text-muted text-center">
               {post.createdAt}
