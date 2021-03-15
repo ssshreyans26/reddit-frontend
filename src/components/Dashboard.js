c5d79f2172d6bd2cbed31b0847cab371405cbf65
Shreyans Sonthalia <ssshreyans26@gmail.com>
2021-03-15 23:59:13 +0530

    dashboard ready functionality has to be added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card,Image ,Col} from "react-bootstrap";
+import { Card, Image, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faArrowCircleUp,
@@ -12,19 +12,30 @@ export default class Dashboard extends Component {
     super();
     this.state = {
       postDetails: [],
+      uid: localStorage.getItem("uid"),
+    };
+    const updateVote = () => {
+      if (!localStorage.getItem("uid")) {
+        //redirect to login page
+      } else {
+        //api call
+        // update icon to green/red
+        //upadte the number (basically set state again)
+      }
     };
   }
   componentDidMount() {
-    fetch("https://obscure-journey-24994.herokuapp.com/feed",{  
-    mode: 'cors',
-    headers: {
-      'Access-Control-Allow-Origin':'*'
-    }}).then((response) => {
+    fetch("https://obscure-journey-24994.herokuapp.com/feed", {
+      mode: "cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    }).then((response) => {
       console.warn(response);
       response.json().then((result) => {
         this.setState({
           postDetails: result,
-        })
+        });
         console.warn(this.state.postDetails);
       });
     });
@@ -32,38 +43,48 @@ export default class Dashboard extends Component {
   render() {
     return (
       <div className="container">
-        {(this.state.postDetails).map((post) => (
-            
+        {this.state.postDetails.map((post) => (
           <Card border="light" bg="dark" text="light">
             <Card.Header as="h3" className="">
-              
-              
-{            (post.test.length!==0)
-             
- ? <Col xs={6} md={6}> <Image src={post.test[0].image} roundedCircle /> {post.test[0].displayName}     </Col> 
- :  <Col xs={6} md={4}> <Image src="{post.test[0].image}" roundedCircle />    </Col>
-            }
+              {post.test.length !== 0 ? (
+                <Col xs={6} md={6}>
+                  {" "}
+                  <Image src={post.test[0].image} roundedCircle />{" "}
+                  {post.test[0].displayName}{" "}
+                </Col>
+              ) : (
+                <Col xs={6} md={4}>
+                  {" "}
+                  <Image src="{post.test[0].image}" roundedCircle />{" "}
+                </Col>
+              )}
             </Card.Header>
 
-              
             <Card.Body>
               <Card.Title>{post.caption}</Card.Title>
               {/* <Card.Img variant="top" src="{post.Location}" /> */}
               <Card.Text>{post.desc}</Card.Text>
               {/* <Button variant="light">Go somewhere</Button> */}
-              
+
               <FontAwesomeIcon
                 className="mr-1"
                 size="2x"
                 icon={faArrowCircleUp}
+                onClick={this.updateVote}
               />
               <span className="text-center mx-2 mb-2">{post.votes}</span>
               <FontAwesomeIcon
                 className="mr-1"
                 size="2x"
                 icon={faArrowCircleDown}
+                onClick={this.updateVote}
               />
-              <FontAwesomeIcon className="ml-0" size="2x" icon={faCommentDots} />
+              <FontAwesomeIcon
+                onClick={/*redirect to single post page*/}
+                className="ml-0"
+                size="2x"
+                icon={faCommentDots}
+              />
             </Card.Body>
             <Card.Footer className="text-muted text-center">
               {post.createdAt}
