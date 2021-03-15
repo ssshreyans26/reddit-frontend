40bced6ef67f981a4ba2693acbda737e9c780bfc
Shreyans Sonthalia <ssshreyans26@gmail.com>
2021-03-15 17:22:37 +0530

    auth func added small change

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -10,7 +10,7 @@ export default function Auth() {
             "dob":dob
 
         }
-        fetch("http://localhost:3000/rel/register",{  
+        fetch("http://localhost:3000/auth/register",{  
           method: 'POST', 
           mode: 'cors',
           headers: {
