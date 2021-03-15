534c7508649ad8124da68b6997c319c4ecabf6ae
Shreyans Sonthalia <ssshreyans26@gmail.com>
2021-03-15 18:25:36 +0530

    create posts some errors resolved

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -32,16 +32,16 @@ export default function CreatePost() {
             <Form>
                 <Form.Group as={Col}>
                 <Form.Label>Caption</Form.Label>
-                <Form.Control type="String" placeholder="Enter Caption" required/>
+                <Form.Control onChange={(e) => setCaption(e.target.value)} type="String" placeholder="Enter Caption" required/>
                 </Form.Group>
 
                 <Form.Group as={Col}>
                 <Form.Label >Description</Form.Label>
-                <Form.Control  as="textarea" rows={3} type="String" placeholder="Enter Description" />
+                <Form.Control  onChange={(e) => setDescription(e.target.value)} as="textarea" rows={3} type="String" placeholder="Enter Description" />
             {/* <Form.Control className="mb-2" type="file" className="mr-3"/> */}
                 </Form.Group>
             </Form>
-            <Button variant="dark" type="submit">
+            <Button onClick={submitFormData(caption,description)} variant="dark" type="submit">
                 Submit
             </Button>
         </Form>
