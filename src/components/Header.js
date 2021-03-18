eaa05beb09eeec4d26cd30aa4a01cd1c9c05c26d
Shreyans Sonthalia <ssshreyans26@gmail.com>
2021-03-18 23:29:41 +0530

    before deployment again

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     return (
       <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
             <LinkContainer to="/">
-  <Navbar.Brand>App Name</Navbar.Brand>
+  <Navbar.Brand>Anonymous</Navbar.Brand>
   </LinkContainer>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
