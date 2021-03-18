505f51e2b5772d89a4b3aba35eff372604e55190
Shreyans Sonthalia <ssshreyans26@gmail.com>
2021-03-18 10:40:49 +0530

    leaderboard done

---
+++ src/components/Leaderboard.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Table } from "react-bootstrap-table";
+
+
+
+export default function Leaderboard() {
+const [leaderboard,setLeaderboardDetails] = useState()
+const getLeaderboardData = () => {
+    var uid = localStorage.getItem('uid');
+     console.log("uid",uid) 
+    fetch("http://localhost:3000/leaderboard", {
+        mode: "cors",
+        method:"GET",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'uid':uid
+        },
+      }).then((response) => {
+        console.warn(response);
+        response.json().then((result) => {
+            console.warn(result)
+            setLeaderboardDetails(result)
+            } 
+        );
+      })
+    }
+
+    React.useEffect(() => {
+        getLeaderboardData();
+        },[]);
+
+return (
+        <div>
+        <Table striped bordered hover variant="dark">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+  {leaderboard.map((item,key) => (
+    <tr>
+      <td>{key}</td>
+      <td>{/*item.key*/}</td>
+      <td>{/*item.key*/}</td>
+      <td>{/*item.key*/}</td>
+    </tr>
+  ))}
+  </tbody>
+</Table>    
+
+        </div>
+    )
+}
